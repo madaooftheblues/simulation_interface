68e2fe2cf30a6e7cbb6d0366a558ad5f1c7f5ce1
CJ071 <anomosc@gmail.com>
2023-11-08 00:41:23 +0500

    Add Robot.js

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,8 +1,82 @@
 import PausePlay from './PausePlay';
+import Robot from './Robot';
+import Object from './Object';
+import Task from './Task';
+import World from './World';
+
 const Main = () => {
+  const robotData = {
+    robotID: 1,
+    robotName: 'Robot 1',
+    batteryLevel: 80,
+    gripperType: 'Type A',
+    currentPosition: 'Location A',
+    defaultPosition: 'Location B',
+  };
+
+   const objectsData = [
+    {
+      objectID: 1,
+      objectName: 'Object 1',
+      objectLocation: 'Location X',
+    },
+    {
+      objectID: 2,
+      objectName: 'Object 2',
+      objectLocation: 'Location Y',
+    }
+  ];
+
+
+  const tasksData = [
+    {
+      taskID: 1,
+      taskTitle: 'Task 1',
+      taskDetails: 'Details of Task 1',
+      robotID: 1,
+      taskStatus: false,
+    },
+    {
+      taskID: 2,
+      taskTitle: 'Task 2',
+      taskDetails: 'Details of Task 2',
+      robotID: 1,
+      taskStatus: true,
+    }
+  ];
+
+  const worldData = {
+    worldID: 1,
+    worldName: 'World 1',
+    numRobots: 5,
+    numObjects: 10,
+    currentTask: 'Task 1',
+    humanAgents: 3,
+  };
+
   return (
-    <>
-      <PausePlay />
+    <> 
+      <PausePlay /> 
+
+      <div className='robot-section'>
+      <Robot robot={robotData} />
+      </div>
+
+      <div className='objects-section'>
+      {objectsData.map((object, index) => (
+        <Object key={index} object={object} />
+      ))}
+      </div>
+
+        <div className='tasks-section'>
+      {tasksData.map((task, index) => (
+        <Task key={index} task={task} />
+      ))}
+      </div>
+
+      <div className='world-section'>
+      <World world={worldData} />
+      </div>
     </>
   );
 };
